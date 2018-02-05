9e1cb621259bc17e3ac4234788dcd17c54bb807a
Project Management-01 <pm01@Nasr.ir>
2018-02-05 09:54:37 +0330

    Added title for filters

--- src/components/SelectFilter.js
+++ src/components/SelectFilter.js
@@ -1,15 +1,21 @@
 // import * as $ from 'jquery'
 
 import Select from '../elements/Select';
+import Elem from '../elements/Element';
 
 export default function SelectFilter(parent, name, options){
+    let wrapper = Elem({ type: 'div' });
+
+    let title = Elem({ type: 'span', title: name });
+    wrapper.appendChild(title);
 
     let selectList = Select({ options });
+    wrapper.appendChild(selectList);
 
     selectList.onchange = ({ target: { value } }) => {
         parent.changeFilter({ name, value });
     };
 
-    parent.appendChild(selectList);
+    parent.appendChild(wrapper);     // [div [span] [select]] // cljs syntax
 }
 
